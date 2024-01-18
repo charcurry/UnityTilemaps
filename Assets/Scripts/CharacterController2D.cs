3052f7a8955de880075965d28d41a1ed046dcaa1
Charlie <charlieryancurry@gmail.com>
2024-01-18 10:19:27 -0400

    Initial commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+
+    public float moveSpeed = 5.0f;
+
+    Vector2 movement;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
